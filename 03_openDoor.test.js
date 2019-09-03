218f9c7b988e66eddd13d1a1ff9996de58eb0fc9
lDoughnutl <sam_harvey98@live.com.au>
2019-09-03 10:20:54 +1000

    Challenge 3 finished

--- 03_openDoor.test.js
+++ 03_openDoor.test.js
@@ -1,6 +1,6 @@
 const openDoor = require("./03_openDoor").openDoor
 
-function makeItem(name, hasCoffee) {
+function makePerson(name, hasCoffee) {
     item = new Object()
     item.name = name
     item.hasCoffee = hasCoffee
@@ -9,30 +9,30 @@ function makeItem(name, hasCoffee) {
 
 describe('openDoor', function () {
 
-    let hash = []
+    let listOfPeople = []
     test('should return true if "Simon" and hasCoffee with single object', function () {
-        hash = []
-        hash.push(makeItem('Simon', true))
-        expect(openDoor(hash)).toBe(true)
+        listOfPeople = []
+        listOfPeople.push(makePerson('Simon', true))
+        expect(openDoor(listOfPeople)).toBe(true)
     })
     test('should return true if "Simon" and hasCoffee with multiple objects', function () {
-        hash = []
-        hash.push(makeItem('Simon', true))
-        hash.push(makeItem('Scott', true))
-        expect(openDoor(hash)).toBe(true)
+        listOfPeople = []
+        listOfPeople.push(makePerson('Simon', true))
+        listOfPeople.push(makePerson('Scott', true))
+        expect(openDoor(listOfPeople)).toBe(true)
     })
     test('should return false if !"Simon" and hasCoffee', function () {
-        hash = []
-        hash.push(makeItem('Scott', true))
-        expect(openDoor(hash)).toBe(false)
+        listOfPeople = []
+        listOfPeople.push(makePerson('Scott', true))
+        expect(openDoor(listOfPeople)).toBe(false)
     })
     test('should return false if "Simon" and !hasCoffee', function () {
-        hash = []
-        hash.push(makeItem('Simon', false))
-        expect(openDoor(hash)).toBe(false)
+        listOfPeople = []
+        listOfPeople.push(makePerson('Simon', false))
+        expect(openDoor(listOfPeople)).toBe(false)
     })
     test('should return false if empty array passed', function () {
-        hash = []
-        expect(openDoor(hash)).toBe(false)
+        listOfPeople = []
+        expect(openDoor(listOfPeople)).toBe(false)
     })
 })
